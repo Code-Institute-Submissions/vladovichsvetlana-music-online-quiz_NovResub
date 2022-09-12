3fdfcca9422c5a5fad41f746b79b748d9b50c19c
vladovichsvetlana <vladovichsvetlana@gmail.com>
2022-09-12 20:39:54 +0000

    adding question 2 to java script

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,18 @@ var quiz = {
     "status": ""
     },
     {
+    "id": question-2,
+    "question": "Which Beatles N.1 featured the word Yellow in the title?",
+    "options": [
+    {
+    "a": "&lt;Red submarine&gt;",
+    "b": "&lt;Yellow submarine&gt;",
+    "c": "&lt;Blue submarine&gt;",
+    "d": "&lt;Green submarine&gt;"
+    }
+    ],
+    "answer": "&lt;Yellow submarine&gt;",
+    "score": 0,
+    "status": ""
+    },
+    {
