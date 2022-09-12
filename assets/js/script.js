bc464afb39456cbaebaa9ac83e4d6241def8fa56
vladovichsvetlana <vladovichsvetlana@gmail.com>
2022-09-12 20:28:48 +0000

    adding java script for question 1

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var quiz = {
+    "JS": [
+    {
+    "id": question-1,
+    "question": ""The Winner Takes It All" was yet another N.1 of which group?",
+    "options": [
+    {
+    "a": "&lt;Abba&gt;",
+    "b": "&lt;Nirvana&gt;",
+    "c": "&lt;Metallica&gt;",
+    "d": "&lt;Guns and Roses&gt;"
+    }
+    ],
+    "answer": "&lt;Abba&gt;",
+    "score": 0,
+    "status": ""
+    },
+    {
