0f1c7c9bd11fcbd507dc30dc6c7e6af2ffd2d675
vladovichsvetlana <vladovichsvetlana@gmail.com>
2022-09-12 20:46:12 +0000

    adding questions 3, 4, 5 to java script

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,3 +31,48 @@ var quiz = {
     "status": ""
     },
     {
+    "id": question-3,
+    "question": "George Michael first hit N.1 as a member of which duo?",
+    "options": [
+    {
+    "a": "&lt;Wham!&gt;",
+    "b": "&lt;Kiss&gt;",
+    "c": "&lt;Damn!&gt;",
+    "d": "&lt;Hello!&gt;"
+    }
+    ],
+    "answer": "&lt;Wham&gt;",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": question-4,
+    "question": "(Everything I Do) I Do it for You! was a monster hit of whom?",
+    "options": [
+    {
+    "a": "&lt;Bryan Adams&gt;",
+    "b": "&lt;Elton John&gt;",
+    "c": "&lt;Sting&gt;",
+    "d": "&lt;Michael Jackson&gt;"
+    }
+    ],
+    "answer": "&lt;Bryan Adams&gt;",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": question-5,
+    "question": "Who was the lead singer with the Police?",
+    "options": [
+    {
+    "a": "&lt;Paul McCartney&gt;",
+    "b": "&lt;Sting&gt;",
+    "c": "&lt;Elton John&gt;",
+    "d": "&lt;Michael Jackson&gt;"
+    }
+    ],
+    "answer": "&lt;Sting&gt;",
+    "score": 0,
+    "status": ""
+    },
+    {
