7c3f0646f73e23c43d0a18b60d43bb1fffe5004d
vladovichsvetlana <vladovichsvetlana@gmail.com>
2022-09-21 20:46:24 +0000

    deleting questions.js section and updating quiz.js section

--- assets/js/questions.js
+++
@@ -1,9 +0,0 @@
-function question (text, options, answer) {
-    this.text = text;
-    this.options= options;
-    this.answer = answer;
-}
-
-question.prototype.correctAnswer = function(option) {
-    return option === this.answer;
-}
